262af696c4e1b0dc5f0035226e80a923f866060e
Tommy-Lim <tlim24@gmail.com>
2017-02-26 15:31:28 -0800

    added beacon

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ $( document ).ready(function() {
     window.sr = ScrollReveal({
       duration: 900
     });
-    sr.reveal('.star-light');
-    sr.reveal('.star-primary');
-    sr.reveal('.resume-container');
-    sr.reveal('.tech-icons', 50);
-    sr.reveal('.portfolio-item', 50);
+    // sr.reveal('.star-light');
+    sr.reveal('header');
+    // sr.reveal('.star-primary');
+    // sr.reveal('.resume-container');
+    sr.reveal('.tech-icons', 75);
+    sr.reveal('.portfolio-item', 75);
+    sr.reveal('.footer-col a', 75);
 });
 
 (function($) {
